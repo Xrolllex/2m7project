ecc356e6e0da01a6cfe87b520ae9316956106bf4
Xrolllex <karolkaczanowski@wp.pl>
2024-08-20 22:08:13 +0200

    Commit

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -5,33 +5,33 @@ import stairs from "../../images/norman-brown-FVaimgno3dw-unsplash-scaled.jpg";
 const Form = () => {
   return (
     <div className={scss.wrapper}>
-    <div className={scss.container}>
-      <img src={stairs} alt="stairs" width="45%" height="auto" />
-      <div className={scss.formWrapper}>
-        <h3>Skontaktuj się z nami i stwórz swoje wymarzone wnętrze</h3>
-        <form className={scss.form}>
-          <div className={scss.input_container}>
-            <label htmlFor="name">Imię</label>
-            <input type="text" id="name" />
-          </div>
-          <div className={scss.input_container}>
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" />
-          </div>
-          <div className={scss.input_container}>
-            <label htmlFor="subject">Temat</label>
-            <input type="text" id="subject" />
-          </div>
-          <div className={scss.input_container}>
-            <label htmlFor="message">Twoja wiadomość</label>
-            <textarea id="message"></textarea>
-          </div>
-          <div className={scss.input_container}>
-            <button className={scss.btn} type="submit">Wyślij</button>
-          </div>
-        </form>
+      <div className={scss.container}>
+        <img src={stairs} alt="stairs" width="25%" height="auto" />
+        <div className={scss.formWrapper}>
+          <h3>Skontaktuj się z nami i stwórz swoje wymarzone wnętrze</h3>
+          <form>
+            <div className={scss.input_container}>
+              <label htmlFor="name">Imię</label>
+              <input type="text" id="name" />
+            </div>
+            <div className={scss.input_container}>
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" />
+            </div>
+            <div className={scss.input_container}>
+              <label htmlFor="subject">Temat</label>
+              <input type="text" id="subject" />
+            </div>
+            <div className={scss.input_container}>
+              <label htmlFor="message">Twoja wiadomość</label>
+              <textarea id="message"></textarea>
+            </div>
+            <div className={scss.input_container}>
+              <button className={scss.btn} type="submit">Wyślij</button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 }

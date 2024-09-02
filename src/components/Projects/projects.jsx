1ee120f710895fa038ee17ce0b3c54050eb19478
Xrolllex <karolkaczanowski@wp.pl>
2024-09-02 20:45:14 +0200

    commit

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
 import scss from "../Projects/project.module.scss"
 
 function Projects() {
     const [projects, setProjects] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [photoIndex, setPhotoIndex] = useState(0);
 
     useEffect(() => {
         fetch('http://localhost:3001/api/projects')
@@ -14,15 +18,41 @@ function Projects() {
             .catch(error => console.error('Błąd pobierania danych:', error));
     }, []); 
 
+    const openLightbox = (index) => {
+        setPhotoIndex(index);
+        setIsOpen(true);
+    };
+
+    const slides = projects.map((project) => ({
+        src: project.imagePath,
+        alt: project.name,
+    }));
+
     return (
-        <div className={scss.project_grid}>
-            {projects.map(project => (
-                <div key={project._id} className={scss.project_item}>
-                    <img src={project.imagePath} alt={project.name} />
-                    <h3>{project.name}</h3>
-                    <p className={scss.description}>{project.description}</p>
-                </div>
-            ))}
+        <div>
+            <h2>Moje projekty</h2>
+            <div className={scss.project_grid}>
+                {projects.map((project, index) => (
+                    <div key={project._id} className={scss.project_item}>
+                        <img 
+                            src={project.imagePath} 
+                            alt={project.name} 
+                            onClick={() => openLightbox(index)} // Otwórz lightbox po kliknięciu w obraz
+                        />
+                        <h3>{project.name}</h3>
+                    </div>
+                ))}
+            </div>
+
+            {isOpen && (
+                <Lightbox
+                    open={isOpen}
+                    close={() => setIsOpen(false)}
+                    slides={slides}
+                    index={photoIndex}
+                    onIndexChange={setPhotoIndex}
+                />
+            )}
         </div>
     );
 }
--- src/components/Why/Icons.jsx
+++ src/components/Why/Icons.jsx
@@ -45,6 +45,7 @@ const Icons = () => {
             </li>
             </div>
             <div className={scss.forwhat}>
+                <hr />
                 <h2>Architekt wnętrz – dla firm i osób prywatnych</h2>
                 <p>
                 2m7Project spełnia marzenia o pięknych i funkcjonalnych wnętrzach – domowych i biurowych. Współpracujemy z firmami z Warszawy i mieszkańcami całego województwa Mazowieckiego, realizując projekty wnętrzarskie zarówno w obiektach deweloperskich, jak i robiąc metamorfozy mieszkań. Gdy zmieniają się Twoje potrzeby lub koncepcja nieruchomości, jesteśmy gotowi na zaprojektowanie wnętrz, które sprostają wszystkim oczekiwaniom.
